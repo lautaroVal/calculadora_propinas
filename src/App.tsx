6be82d4c812aa2516f25d4aaa9b784f125ab79c8
lautaroVal <valenzuelalautaro@yahoo.com>
2024-11-05 16:04:31 -0300

    Agregando los contenedores principales

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import { menuItems } from "./data/db"
 function App() {
 
   console.log(menuItems);
-  
+
   return (
     <>
-    <header className="bg-teal-400 py-5">
-      <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumos</h1>
-    </header>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumos</h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2 className="">Menú</h2>
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
 
     </>
   )
