83cdf4f0754442813702008de1f2f23112395f78
lautaroVal <valenzuelalautaro@yahoo.com>
2024-11-05 15:50:17 -0300

    Agregando productos DB y type

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-
+import { menuItems } from "./data/db"
 
 function App() {
 
+  console.log(menuItems);
+  
   return (
     <>
     <header className="bg-teal-400 py-5">
       <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumos</h1>
     </header>
-    
+
     </>
   )
 }
