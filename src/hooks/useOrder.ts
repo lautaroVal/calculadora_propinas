a66cd07a8a3881f74cf424367d617a53d865afa9
lautaroVal <valenzuelalautaro@yahoo.com>
2025-01-17 16:49:28 -0300

    Agregando el signature a la función de agregar articulo con TypeScript y  Añadiendo elementos al state de Orden

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,12 +1,19 @@
 import { useState } from "react"
-import { OrderItem } from "../types"
+import { MenuItem, OrderItem } from "../types"
 
 export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
     
-    const addItem = () => {
-        console.log('Agregando...')
+    const addItem = (item: MenuItem) => {
+
+        const newItem = {...item, quantity:1}
+        setOrder([...order, newItem])
+
+
     }
+
+    console.log(order);
+    
 
     return {
         addItem
