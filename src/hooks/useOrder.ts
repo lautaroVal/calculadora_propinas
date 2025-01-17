803c8af43cfb57af64f210407c631ab71bd8f46a
lautaroVal <valenzuelalautaro@yahoo.com>
2025-01-17 18:39:45 -0300

    Eliminando articulos de la orden

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -17,9 +17,14 @@ export default function useOrder() {
         }
     }
 
+    const removeItem = (id: MenuItem['id']) => {
+                setOrder(order.filter( item => item.id !== id))
+    }
+
     return {
         order,
-        addItem
+        addItem,
+        removeItem
     }
 
 }
