77132ad192182464f4744b865e2df5f3275b656b
lautaroVal <valenzuelalautaro@yahoo.com>
2025-01-18 01:41:02 -0300

    Calculando el Consumo

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,11 +1,21 @@
+import { useMemo } from "react"
+import { OrderItem } from "../types"
+import { formatCurrency } from "../helpers"
 
-export default function OrderTotals() {
+type OrderTotalsProps = {
+    order: OrderItem[]
+}
+
+export default function OrderTotals({order} : OrderTotalsProps) {
+
+    const subtotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0) , [order])
+
   return (
     <>
     <div className="space-y-3">
         <h2 className="font-black text-2xl">Totales y Propina</h2>
         <p>Subtotal a Pagar: {''}
-            <span className="font-bold">$0</span>
+            <span className="font-bold">{ formatCurrency(subtotalAmount)}</span>
         </p>
 
         <p>Propina: {''}
